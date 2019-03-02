7286d12937b62378e9c7912b35f4e12ef071c35f
meet <meetsavaliya998@gmail.com>
2019-03-02 01:35:00 -0700

    Sample generated and reading the files is done

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+         ArrayList<String> sample;
+         String initial=new String();
+         File fl;
+         Scanner out;
+         for(int i=1;i<=10;i++) {
+             fl = new File(i+".txt");
+             out = new Scanner(fl);
+             initial=out.nextLine();
+             System.out.println(initial);
+         }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
